783b5a29970009e5ab6e464c2c5795961a411104
luirusso <luirusso_@hotmail.it>
2021-11-29 15:18:13 +0100

    added search form

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="search-container">
+            <form>
+                <input type="text" placeholder="Cerca film e serie TV"/>
+                <button type="submit">CERCA</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
