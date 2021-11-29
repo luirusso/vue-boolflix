f8bf929d99f8b26d10615f26669b0da089bb3060
luirusso <luirusso_@hotmail.it>
2021-11-29 17:18:40 +0100

    commit before revert

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
 <script>
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
-import axios from "axios";
 
 export default {
     name: "App",
@@ -38,29 +37,11 @@ export default {
         },
     },
     methods: {
-        getMovie() {
-            /**
-             * Get movies list from api
-             */
-            axios
-                .get("https://api.themoviedb.org/3/movie/550", {
-                    params: {
-                        api_key: "56dd66302a0e2a5c6c9bbd2082ac8c6d",
-                        query: this.inputSearch,
-                        language: "it-IT",
-                    },
-                })
-                .then((result) => {
-                    console.log(result.data);
-                    this.moviesList = result.data;
-                })
-                .catch((error) => console.log(error));
-        },
-    },
-    performSearch(text) {
+            performSearch(text) {
         console.log(text);
         this.inputSearch = text;
     },
+  },
 };
 </script>
 
