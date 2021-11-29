4ac46c7efd96368bb7c04b8acc307f6030e5fde7
luirusso <luirusso_@hotmail.it>
2021-11-29 17:24:59 +0100

    reverted commits

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,45 +1,20 @@
 <template>
-    <div class="container">
-        <ul>
-            <li></li>
-        </ul>
-    </div>
+  <div class="container">
+      <ul>
+          <li>
+              
+          </li>
+      </ul>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
-    name: "Main",
+    name: 'Main',
 
-    data() {
-        return {
-            inputSearch: '',
-            moviesList: [],
-        };
-    },
-    
-    methods: {
-        getMovie() {
-            /**
-             * Get movies list from api
-             */
-            axios
-                .get("https://api.themoviedb.org/3/movie/550", {
-                    params: {
-                        api_key: "56dd66302a0e2a5c6c9bbd2082ac8c6d",
-                        query: this.inputSearch,
-                        language: "it-IT",
-                    },
-                })
-                .then((result) => {
-                    console.log(result.data);
-                    this.moviesList = result.data;
-                })
-                .catch((error) => console.log(error));
-        },
-    },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+</style>
