55424bf591fd9da3448fdd0fe7313eddc32ee546
luirusso <luirusso_@hotmail.it>
2021-11-29 17:22:32 +0100

    Revert "added emit inside header"
    This reverts commit 94a84b111225ad93f3b6740070ae72747250533a.

--- src/App.vue
+++ src/App.vue
@@ -5,42 +5,6 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Main from "./components/Main.vue";
-
-export default {
-    name: "App",
-    components: {
-        Header,
-        Main,
-    },
-    data() {
-        return {
-            inputSearch: "",
-            moviesList: [],
-        };
-    },
-    created() {
-        this.getMovie();
-    },
-    computed: {
-        filteredSearch() {
-            if (this.inputSearch === "") {
-                return this.moviesList;
-            }
-
-            return this.moviesList.filter((item) => {
-                return item.title.includes(this.inputSearch);
-            });
-        },
-    },
-    methods: {
-            performSearch(text) {
-        console.log(text);
-        this.inputSearch = text;
-    },
-  },
-};
 import Header from './components/Header.vue'
 
 export default {
