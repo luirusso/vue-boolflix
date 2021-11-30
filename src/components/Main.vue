bda913ccda63cc3f460150e4dbbe31226e2b17d7
luirusso <luirusso_@hotmail.it>
2021-11-30 16:49:28 +0100

    added some style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,57 +2,61 @@
     <div class="container">
         <ul>
             <li v-for="(movie, index) in moviesArray" :key="`movie-${index}`">
-                <div>
-                    <img
-                        v-if="movie.poster_path !== null"
-                        :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`"
-                        alt=""
-                    />
-                    <img v-else src="../assets/imageError.png" alt="" />
-                </div>
+                <div class="card-content">
+                    <div>
+                        <img
+                            v-if="movie.poster_path !== null"
+                            :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`"
+                            alt=""
+                        />
+                        <img v-else src="../assets/imageError.png" alt="" />
+                    </div>
 
-                <div>Titolo: {{ movie.title }}</div>
-                <div>Titolo originale: {{ movie.original_title }}</div>
-                <div>Voto: {{ movie.vote_average }}</div>
-                <div>
-                    <img
-                        class="flag"
-                        v-if="movieImageSelector(movie.original_language)"
-                        :src="
-                            require(`../assets/${movie.original_language}.png`)
-                        "
-                        alt=""
-                    />
+                    <div>Titolo: {{ movie.title }}</div>
+                    <div>Titolo originale: {{ movie.original_title }}</div>
+                    <div>Voto: {{ movie.vote_average }}</div>
+                    <div>
+                        <img
+                            class="flag"
+                            v-if="movieImageSelector(movie.original_language)"
+                            :src="
+                                require(`../assets/${movie.original_language}.png`)
+                            "
+                            alt=""
+                        />
+                    </div>
+                    <div>Lingua originale: {{ movie.original_language }}</div>
                 </div>
-                <div>Lingua originale: {{ movie.original_language }}</div>
             </li>
 
             <li
                 v-for="(series, index) in tvSeriesArray"
                 :key="`series-${index}`"
             >
-                <div>
-                    <img
-                        v-if="series.poster_path !== null"
-                        :src="`https://image.tmdb.org/t/p/w342${series.poster_path}`"
-                        alt=""
-                    />
-                    <img v-else src="../assets/imageError.png" alt="" />
+                <div class="card-content">
+                    <div>
+                        <img
+                            v-if="series.poster_path !== null"
+                            :src="`https://image.tmdb.org/t/p/w342${series.poster_path}`"
+                            alt=""
+                        />
+                        <img v-else src="../assets/imageError.png" alt="" />
+                    </div>
+                    <div>Titolo: {{ series.name }}</div>
+                    <div>Titolo originale: {{ series.original_name }}</div>
+                    <div>Voto: {{ series.vote_average }}</div>
+                    <div>
+                        <img
+                            class="flag"
+                            v-if="movieImageSelector(series.original_language)"
+                            :src="
+                                require(`../assets/${series.original_language}.png`)
+                            "
+                            alt=""
+                        />
+                    </div>
+                    <div>Lingua originale: {{ series.original_language }}</div>
                 </div>
-                <div>Titolo: {{ series.name }}</div>
-                <div>Titolo originale: {{ series.original_name }}</div>
-                <div>Voto: {{ series.vote_average }}</div>
-                <div>
-                    <img
-                        class="flag"
-                        v-if="movieImageSelector(series.original_language)"
-                        :src="
-                            require(`../assets/${series.original_language}.png`)
-                        "
-                        alt=""
-                    />
-                </div>
-                <div>Lingua originale: {{ series.original_language }}</div>
             </li>
         </ul>
     </div>
@@ -77,7 +81,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.flag {
-    max-width: 30px;
+ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    li {
+        padding: 1rem 1.5rem;
+        background-color: lightgray;
+    }
+    .flag {
+        max-width: 30px;
+    }
 }
 </style>
